e73bc8dd7149f63e8ae4056cc6f92de0f7aaceb8
gbz3 <1808483+gbz3@users.noreply.github.com>
2024-09-14 14:53:26 +0900

    add reporting annotation

--- my-app/src/test/java/com/github/gbz3/try_jqwik/PropertyBasedTest.java
+++ my-app/src/test/java/com/github/gbz3/try_jqwik/PropertyBasedTest.java
@@ -5,11 +5,13 @@ import org.assertj.core.api.*;
 
 class PropertyBasedTest {
 
+    @Report({Reporting.GENERATED, Reporting.FALSIFIED})
     @Property
     boolean alwaysPositive(@ForAll int anInteger) {
         return Math.abs(anInteger) >= 0;
     }
 
+    @Report({Reporting.GENERATED, Reporting.FALSIFIED})
     @Property
     void checkLength(@ForAll String str1, @ForAll String str2) {
         final var str = str1 + str2;
