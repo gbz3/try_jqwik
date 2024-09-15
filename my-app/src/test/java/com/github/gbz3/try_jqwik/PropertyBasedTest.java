b2771e4a993fc18f27a32f3fbef9c5b48332b05b
gbz3 <1808483+gbz3@users.noreply.github.com>
2024-09-15 09:15:54 +0900

    successgit add .

--- my-app/src/test/java/com/github/gbz3/try_jqwik/PropertyBasedTest.java
+++ my-app/src/test/java/com/github/gbz3/try_jqwik/PropertyBasedTest.java
@@ -1,19 +1,19 @@
 package com.github.gbz3.try_jqwik;
 
 import net.jqwik.api.*;
+import net.jqwik.api.constraints.IntRange;
+import net.jqwik.api.constraints.NotEmpty;
 import org.assertj.core.api.*;
 
 class PropertyBasedTest implements AutoCloseable {
 
-    @Report({Reporting.GENERATED, Reporting.FALSIFIED})
     @Property
-    boolean alwaysPositive(@ForAll int anInteger) {
+    boolean alwaysPositive(@ForAll @IntRange(min = Integer.MIN_VALUE + 1) int anInteger) {
         return Math.abs(anInteger) >= 0;
     }
 
-    @Report({Reporting.GENERATED, Reporting.FALSIFIED})
     @Property
-    void checkLength(@ForAll String str1, @ForAll String str2) {
+    void checkLength(@ForAll @NotEmpty String str1, @ForAll @NotEmpty String str2) {
         final var str = str1 + str2;
         Assertions.assertThat(str.length()).isGreaterThan(str1.length());
         Assertions.assertThat(str.length()).isGreaterThan(str2.length());
