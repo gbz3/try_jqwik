b4ea30d96ba12da420a7dab3b5a5ce5f838b5e02
gbz3 <1808483+gbz3@users.noreply.github.com>
2024-09-16 08:28:19 +0900

    compile ok

--- my-app/src/test/java/com/github/gbz3/try_jqwik/CsvTest.java
+++ my-app/src/test/java/com/github/gbz3/try_jqwik/CsvTest.java
@@ -8,17 +8,42 @@ import java.util.Map;
 
 public class CsvTest implements AutoCloseable {
 
-    CsvEncoder _enc = new CsvEncoder() {};;
-    CsvDecoder _dec = new CsvDecoder() {};;
+    CsvEncoder _enc = new CsvEncoder() {};
+    CsvDecoder _dec = new CsvDecoder() {};
 
     @Property
-    void symmetric(@ForAll List<Map<String, String>> aCsv) {
+    void symmetric(@ForAll("csvData") List<Map<String, String>> aCsv) {
         final var encoded = _enc.encode(aCsv);
         Assertions.assertThat(aCsv).isEqualTo(_dec.decode(encoded));
     }
 
+    @Provide
+    Arbitrary<List<Map<String, String>>> csvData(@ForAll("csvField") String aField) {
+        var size = Arbitraries.integers().between(1, 10).sample();
+        var keys = Arbitraries.of(aField).list().ofSize(size).sample();
+        return Arbitraries
+                .maps(Arbitraries.of(keys), Arbitraries.of(aField))
+                .collect(list -> true);
+    }
+
+    @Provide
+    Arbitrary<String> csvField() {
+        var unquotedText = Arbitraries.strings()
+                .alpha()
+                .numeric()
+                .ofMinLength(1);
+
+        var quotableText = Arbitraries.strings()
+                .alpha()
+                .numeric()
+                .withChars('\r', '\n', '"', ',')
+                .ofMinLength(1);
+
+        return Arbitraries.oneOf(unquotedText, quotableText);
+    }
+
     @Override
-    public void close() throws Exception {
+    public void close() {
         // Nothing
     }
 
