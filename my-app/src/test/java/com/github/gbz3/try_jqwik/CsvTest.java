b2a9b77ceb72ec6a5b1589c7fdb967e409a9431c
gbz3 <1808483+gbz3@users.noreply.github.com>
2024-09-16 11:07:19 +0900

    キーの共通化には未対応

--- my-app/src/test/java/com/github/gbz3/try_jqwik/CsvTest.java
+++ my-app/src/test/java/com/github/gbz3/try_jqwik/CsvTest.java
@@ -1,10 +1,12 @@
 package com.github.gbz3.try_jqwik;
 
 import net.jqwik.api.*;
+import net.jqwik.api.statistics.Statistics;
 import org.assertj.core.api.Assertions;
+import org.assertj.core.data.MapEntry;
 
-import java.util.List;
-import java.util.Map;
+import java.util.*;
+import java.util.stream.Collectors;
 
 public class CsvTest implements AutoCloseable {
 
@@ -13,21 +15,17 @@ public class CsvTest implements AutoCloseable {
 
     @Property
     void symmetric(@ForAll("csvData") List<Map<String, String>> aCsv) {
+        Statistics.collect(aCsv.size());
+
         final var encoded = _enc.encode(aCsv);
         Assertions.assertThat(aCsv).isEqualTo(_dec.decode(encoded));
     }
 
     @Provide
-    Arbitrary<List<Map<String, String>>> csvData(@ForAll("csvField") String aField) {
-        var size = Arbitraries.integers().between(1, 10).sample();
-        var keys = Arbitraries.of(aField).list().ofSize(size).sample();
-        return Arbitraries
-                .maps(Arbitraries.of(keys), Arbitraries.of(aField))
-                .collect(list -> true);
-    }
+    Arbitrary<List<Map<String, String>>> csvData() {
+        var sizeNullable = Arbitraries.integers().between(1, 10).sample();
+        var size = Optional.ofNullable(sizeNullable).orElse(1);
 
-    @Provide
-    Arbitrary<String> csvField() {
         var unquotedText = Arbitraries.strings()
                 .alpha()
                 .numeric()
@@ -39,7 +37,12 @@ public class CsvTest implements AutoCloseable {
                 .withChars('\r', '\n', '"', ',')
                 .ofMinLength(1);
 
-        return Arbitraries.oneOf(unquotedText, quotableText);
+        var field = Arbitraries.oneOf(unquotedText, quotableText);
+        //var colNames = field.set().ofSize(size);
+
+        var aMap = Arbitraries.maps(field, field).ofSize(size);
+
+        return aMap.list();
     }
 
     @Override
